1fe8849d6119f8ebcebb64874e3fd1f06aa4ae4f
Lincoln2810 <tya3869@students.fhv.at>
2024-10-22 21:21:47 +0000

    the explanation for the arguments are inplemented

--- mywc.c
+++ mywc.c
@@ -4,16 +4,29 @@
 
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-    int count = 0;
+int main(int argc, char *argv[])
+{
+    int count = 0; 
 
-    if (argc == 2 && argv[1][0] == '-' && argv[1][1] == 'c') {
-        while (getchar() != EOF) {
+    if (argc == 2 && argv[1][0] == '-' && argv[1][1] == 'c')
+    {
+        while (getchar() != EOF)
+        {
             count++;
         }
         printf("\nNumber of characters: %d\n", count);
         return 0;
     }
-    
+
+    else if (argc == 2 && argv[1][0] == '-' && argv[1][1] == 'h')
+    {
+        printf("This program is designed to count the number of characters\n");
+        printf("from standard input (stdin) and print the total count.\n");
+        printf("It also provides help information when a specific command-line argument is passed. \n");
+        printf("Explanation of the arguments:\n");
+        printf("-c\t counts the characters\n");
+        printf("-w\t counts the words\n");
+    }
+
     return 0;
 }
