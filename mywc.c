b559722133752e2167ac820bfcc9016f58f32761
Lincoln2810 <tya3869@students.fhv.at>
2024-10-22 21:48:12 +0000

    the word counter is implemented into the program

--- mywc.c
+++ mywc.c
@@ -4,28 +4,46 @@
 
 #include <stdio.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     int count = 0; 
+    int in_word = 0;
+    char temp;
 
-    if (argc == 2 && argv[1][0] == '-' && argv[1][1] == 'c')
-    {
-        while (getchar() != EOF)
-        {
+    if (argc == 2 && argv[1][0] == '-' && argv[1][1] == 'c') {
+        while (getchar() != EOF) {
             count++;
         }
         printf("\nNumber of characters: %d\n", count);
         return 0;
     }
 
-    else if (argc == 2 && argv[1][0] == '-' && argv[1][1] == 'h')
-    {
+    else if (argc == 2 && argv[1][0] == '-' && argv[1][1] == 'h') {
         printf("This program is designed to count the number of characters\n");
         printf("from standard input (stdin) and print the total count.\n");
-        printf("It also provides help information when a specific command-line argument is passed. \n");
+        printf("It also provides help information when a specific command-line argument is passed.\n");
         printf("Explanation of the arguments:\n");
         printf("-c\t counts the characters\n");
         printf("-w\t counts the words\n");
+    }
+
+    else if (argc == 2 && argv[1][0] == '-' && argv[1][1] == 'w') {
+        while ((temp = getchar()) != EOF) {
+            if (temp == ' ' || temp == '\n' || temp == '\t') {
+                if (in_word == 1) {
+                    count++;
+                    in_word = 0;
+                }
+            } 
+            else {
+                in_word = 1;
+            }
+        }
+
+        if (in_word == 1) {
+            count++;
+        }
+        printf("\nNumber of words: %d\n", count);
+        return 0;
     }
 
     return 0;
